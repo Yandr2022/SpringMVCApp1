c4b1529667a86ffb6e04cf8e1e28270e6a090964
Yandr <102684712+Yandr2022@users.noreply.github.com>
2023-08-16 14:24:05 +0300

    learning Spring MVC: Request parameters, Model

--- src/main/java/by/Yandr2022/springlearn/controller/FirstController.java
+++ src/main/java/by/Yandr2022/springlearn/controller/FirstController.java
@@ -1,18 +1,59 @@
 package by.Yandr2022.springlearn.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
-@GetMapping("/hello")
-    public  String helloPage(){
-return "first/hello";
+    //    @GetMapping("/hello")
+//    public String helloPage(HttpServletRequest request) {
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
+//        System.out.println("Hello "+ name + " " + surname);
+//        return "first/hello";
+//    }
+    @GetMapping("/hello")
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+        model.addAttribute("message", "Hello " + name + " " + surname);
+        return "first/hello";
     }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a") int a,
+                             @RequestParam(value = "b") int b,
+                             @RequestParam(value = "action", required = false) String action,
+                             Model model) {
+        String res;
+        switch (action) {
+            case "multiplication":
+                res = (a * b) + "";
+                break;
+            case "addition":
+                res = (a + b) + "";
+                break;
+            case "subtraction":
+                res = (a - b) + "";
+                break;
+            case "division":
+                res = ((double)a / b) + "";
+                break;
+            default:
+                res = "Wrong action";
+        }
+        model.addAttribute("res", res);
+        return "first/calculator";
+    }
+
     @GetMapping("/goodbye")
-    public  String goodByePage(){
+    public String goodByePage() {
         return "first/goodbye";
     }
 }
--- src/main/java/by/Yandr2022/springlearn/controller/SecondController.java
+++ src/main/java/by/Yandr2022/springlearn/controller/SecondController.java
@@ -2,8 +2,10 @@ package by.Yandr2022.springlearn.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/second")
 public class SecondController {
 @GetMapping("/exit")
     public String exit(){
